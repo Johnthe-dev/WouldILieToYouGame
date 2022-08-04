7ae3556a12b1e99cea0ceeae3be94abae301838b
John Johnson-Rodgers <john@johnthe.dev>
2022-08-03 21:58:32 -0600

    built game api

---
+++ php/public_html/apis/game/index.php
@@ -0,0 +1,90 @@
+<?php
+
+require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
+require_once dirname(__DIR__, 3) . "/Classes/autoload.php";
+require_once("/etc/apache2/game/Secrets.php");
+require_once dirname(__DIR__, 3) . "/lib/xsrf.php";
+require_once dirname(__DIR__, 3) . "/lib/jwt.php";
+require_once dirname(__DIR__, 3) . "/lib/uuid.php";
+
+use JOHNTHEDEV\Game\Game;
+
+/**
+ * api for getting the game state, creating a game, etc.
+ *
+ * @author John Johnson-Rodgers <john@johnthe.dev>
+ */
+
+//verify the session, start if inactive
+if(session_status() !== PHP_SESSION_ACTIVE) {
+    session_start();
+}
+
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try {
+    //grab the mySQL connection
+    $secrets = new \Secrets("var/www/apache/secret/game.ini");
+    $pdo = $secrets->getPdoObject();
+
+    //determine which HTTP method was used
+    $method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
+
+    //sanitize input
+    $gameId = filter_input(INPUT_GET, "gameId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+
+    //check if resourceId is empty and method is delete or put
+    if(($method === "DELETE" || $method === "PUT") && (empty($gameId) === true)) {
+        throw(new InvalidArgumentException("gameId can not be empty when deleting of changing", 400));
+    }
+
+    if($method === "GET") {
+        //set xsrf cookie
+        setXsrfCookie();
+
+        if(isset($gameId) === true) {
+            //get resource by resource id
+            $reply->data = Game::getGameByGameId($pdo, $gameId);
+        }
+
+    } elseif($method === "POST" || $method === "PUT") {
+        //enforce xsrf token
+        verifyXsrf();
+        if(empty($_SESSION["player"]) === true) {
+            throw(new \InvalidArgumentException("You do not have permission to edit or add game", 401));
+        }
+
+        //retrieves JSON package that was sent by the user and stores it in $requestContent using file_get_contents
+        $requestContent = file_get_contents("php://input");
+
+        //Decodes content and stores result in $requestContent
+        $requestObject = json_decode($requestContent);
+
+        if(!property_exists($requestObject, "gameCode")){$requestObject["gameCode"]=NULL;}
+
+        if($method === "POST") {
+            //makes sure required fields are available
+            if(empty($requestObject->gameCode) === true) {
+                throw(new \InvalidArgumentException("The Game Code field is empty.", 400));
+            }
+
+            //create new game and insert it into the database
+            $game = new Game(generateUuidV4(), $requestObject->gameCode, null, null,
+                null, null, 0,0);
+            $game->insert($pdo);
+            //update reply
+            $reply->message = "A new game entry has been created.";
+        }
+    } else {
+        throw (new InvalidArgumentException("Invalid HTTP method request", 405));
+    }
+
+} catch(\Exception | \TypeError $exception) {
+    $reply->status = $exception->getCode();
+    $reply->message = $exception->getMessage();
+}
+header("Content-type: application/json");
+echo json_encode($reply);
---
+++ php/public_html/apis/player-registration/index.php
@@ -0,0 +1,73 @@
+<?php
+
+require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
+require_once dirname(__DIR__, 3) . "/Classes/autoload.php";
+require_once("/etc/apache2/game/Secrets.php");
+require_once dirname(__DIR__, 3) . "/lib/xsrf.php";
+require_once dirname(__DIR__, 3) . "/lib/uuid.php";
+
+use JOHNTHEDEV\Game\{Player};
+
+/**
+ * api for signing-up as a player
+ *
+ * @author John Johnson-Rodgers <john@johnthe.dev>
+ */
+
+//verify session
+if(session_status() !== PHP_SESSION_ACTIVE) {
+    session_start();
+}
+
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try {
+    //connect to mySQL
+    $secrets = new \Secrets("/var/www/apache/secret/game.ini");
+    $pdo = $secrets->getPdoObject();
+
+    //determine what http method was used
+    $method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+    if($method === "POST") {
+        //decode the json and turn it into a php object
+        $requestContent = file_get_contents("php://input");
+        $requestObject = json_decode($requestContent);
+
+        //generate player attributes
+        $playerId = generateUuidV4();
+        $playerGameId = $requestObject->gameId;
+        $playerName = $requestObject->name;
+        $playerTeamNumber = $requestObject->teamNumber;
+        $playerPlayed = false;
+        $playerLastModified = null;
+
+        //create the player object and prepare to insert it into the database
+        $player = new Player(
+            $playerId,
+            $playerGameId,
+            $playerName,
+            $playerTeamNumber,
+            $playerPlayed,
+            $playerLastModified);
+
+        //insert player into database
+        $player->insert($pdo);
+
+        //update reply
+        $reply->message = "You have successfully become a player!";
+    } else {
+        throw(new InvalidArgumentException("Invalid http request", 405));
+    }
+
+} catch(\Exception | \TypeError $exception) {
+    $reply->status = $exception->getCode();
+    $reply->message = $exception->getMessage();
+    $reply->trace = $exception->getTraceAsString();
+}
+
+header("Content-type: application/json");
+echo json_encode($reply);
